735aef5de95ea185dbb2f635357e4b7a3eb4ab68
fb0801 <fbhatti0801@gmail.com>
2024-06-11 18:06:53 +0100

    project setup  styling the sidebar 2

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ https://www.toptal.com/developers/gitignore/
 https://www.youtube.com/watch?v=OHvfgaDl-yY
 
 original repo -> https://github.com/Maclinz/todoapp_fullstack
+
+https://www.toptal.com/developers/gitignore/
--- mytasks/app/Components/Sidebar/Sidebar.tsx
+++ mytasks/app/Components/Sidebar/Sidebar.tsx
@@ -3,13 +3,17 @@
 import { useGlobalState } from '@/app/context/globalProvider'
 import React from 'react'
 import styled from 'styled-components/dist/constructors/styled'
+import Image from 'next/image'
 
 
 function Sidebar() {
 const {theme} = useGlobalState();
   
   return <SidebarStyled theme={theme}>
-      sidebar
+      <div className='profile'>
+        <div className="profile-overlay"></div>
+      <Image width={70} height={70} src='/images/profile.jpg' alt='profile' />
+      </div>
     </SidebarStyled>
   
 }
@@ -18,7 +22,7 @@ const SidebarStyled = styled.nav`
   position: relative;
   width: ${(props) => props.theme.sidebarWidth};
   background-color: ${(props) => props.theme.colorBg2};
-  border-right: 2px solid ${(props) => props.theme.borderColor2}
+  border: 2px solid ${(props) => props.theme.borderColor2}
   border-radius: 1rem;
   ;
 `; 
