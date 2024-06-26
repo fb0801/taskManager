17d96292b454a3b18c8576379d876da0cc2d880f
fb0801 <fbhatti0801@gmail.com>
2024-06-26 17:55:36 +0100

    getting and display tasks

--- mytasks/app/Components/Tasks/Tasks.tsx
+++ mytasks/app/Components/Tasks/Tasks.tsx
@@ -7,11 +7,30 @@ import TaskItem from "../TaskItem/TaskItem";
 import { add, plus } from "@/app/utils/Icons";
 import Modal from "../Modals/Modal";
 
-function Tasks()  {
+
+interface Props {
+  title: string;
+  description: string;
+  tasks: any[];
+}
+
+function Tasks({title, tasks} :Props)  {
     const {theme} = useGlobalState()
   
     return <TaskStyled theme={theme}>
-    <CreateContent />
+      <h1>{title}</h1>
+   <div className="tasks grid">
+    {tasks.map((task) => (
+      <div className="task" key={task._id}>
+        <h2>{task.title}</h2>
+        <p>{task.description}</p>
+        <p>{task.date}</p>
+        <p>{task.completed}</p>
+        <p>{task.important}</p>
+      </div>
+    ))}
+
+   </div>
     </TaskStyled>
 
 }
